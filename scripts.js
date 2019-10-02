0cbc6eec282c58cd33b384f3a6b1db6677f0a2d0
julienbigras <julien.bigras@hotmail.com>
2019-10-02 02:39:07 -0400

    play again button logic almost fully functional

--- scripts.js
+++ scripts.js
@@ -6,6 +6,10 @@ const gameOptions =[
 
 let userChoice = "";
 
+const modalOpen = document.getElementById("openModal");
+
+const label = document.getElementsByClassName("label");
+
 const submit = document.getElementById('submit');
 
 const warning = document.getElementById("warning");
@@ -14,10 +18,12 @@ const results = document.getElementById("resultsContent");
 
 const playAgain = document.getElementById("playAgain");
 
+
+const resultsMessage = document.createElement('p');
+
 // FUNCTION TO OPEN THE USER CHOICE MODAL
 
 const openModal = () => {
-    const modalOpen = document.getElementById("openModal");
     const userPickModal = document.getElementById("userPickModal")
 
     modalOpen.addEventListener("click", () => {
@@ -36,13 +42,17 @@ for (let i = 0; i < radioButtons.length; i++) {
         // assign the value of the selected radio button to the userChoice variable
         userChoice = radioButtons[i].value;
         warning.style.visibility = "hidden";
-        console.log(userChoice);
+        console.log(userChoice, "user choice");
     })
 }
 
 // randomly choose one of the three game options, and assign it to the computerChoice variable
-const computerChoice = gameOptions[Math.floor(Math.random() * gameOptions.length)];
-console.log(computerChoice);
+const getComputerChoice = () => {
+    return gameOptions[Math.floor(Math.random() * gameOptions.length)];
+}
+
+let computerChoice = getComputerChoice();
+console.log(computerChoice, "computer choice");
 
 const startAnimation = () => {
     const hand = document.getElementById('hand');
@@ -86,25 +96,22 @@ const startAnimation = () => {
                     userChoice === "rock" && computerChoice === "scissors" ||
                     userChoice === "paper" && computerChoice === "rock" ||
                     userChoice === "scissors" && computerChoice === "paper") {
-                        const winMessage = document.createElement('p');
-                        winMessage.innerHTML = "Congrats, you won!";
-                        results.appendChild(winMessage);
+                        resultsMessage.innerHTML = "Congrats, you win!";
+                        results.appendChild(resultsMessage);
                         playAgain.style.display = "block";
                 } else if (
                     userChoice === "rock" && computerChoice === "paper" ||
                     userChoice === "paper" && computerChoice === "scissors" ||
                     userChoice === "scissors" && computerChoice === "rock") {
-                        const lossMessage = document.createElement('p');
-                        lossMessage.innerHTML = "Bummer, you lost!";
-                        results.appendChild(lossMessage);
+                        resultsMessage.innerHTML = "Bummer, you lose!";
+                        results.appendChild(resultsMessage);
                         playAgain.style.display = "block";
                 } else if (
                     userChoice === "rock" && computerChoice === "rock" ||
                     userChoice === "paper" && computerChoice === "paper" ||
                     userChoice === "scissors" && computerChoice === "scissors") {
-                        const tieMessage = document.createElement('p');
-                        tieMessage.innerHTML = "It's a tie!";
-                        results.appendChild(tieMessage);
+                        resultsMessage.innerHTML = "It's a tie!";
+                        results.appendChild(resultsMessage);
                         playAgain.style.display = "block";
                 }
             }, 1450)
@@ -116,15 +123,33 @@ const startAnimation = () => {
 startAnimation();
 
 
-// playAgain.addEventListener("click", () => {
-//     hand.setAttribute("src", "./assets/rock.png");
-//     handReverse.setAttribute("src", "./assets/rock-reverse.png");
-//     computerChoice = "";
-//     playAgain.style.display = "none";
-//     hand.classList.remove("handAnimate");
-//     handReverse.classList.remove("handReverseAnimate");
-// })
-
 playAgain.addEventListener("click", () => {
-    document.location.reload();
+    // reset the userChoice and computerChoice variables
+    computerChoice = getComputerChoice();
+    console.log(computerChoice);
+    userChoice = "";
+    // show the openModal button
+    modalOpen.style.visibility = "visible";
+    // reset the radio buttons
+    radioButtons.checked = false;
+
+    // reset the color of the radio button label
+    // for (i = 0; i < label.length; i++) {
+    //     label[i].style.background = "white";
+    // }
+
+    // change the two hand images back to their original source files
+    hand.setAttribute("src", "./assets/rock.png");
+    handReverse.setAttribute("src", "./assets/rock-reverse.png");
+    // remove the animate classes from each of the hand images
+    hand.classList.remove("handAnimate");
+    handReverse.classList.remove("handReverseAnimate");
+    // hide the play again button
+    playAgain.style.display = "none";
+    // remove the previous results message
+    results.removeChild(resultsMessage)
 })
+
+// playAgain.addEventListener("click", () => {
+//     document.location.reload();
+// })
